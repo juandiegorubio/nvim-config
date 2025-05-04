7b77c9420f8c7afc963c5d965e616f3e067c88b0
juandiegorubio <rubio.juandiego.cs@gmail.com>
2025-05-04 11:31:28 +0200

    fix: Remove 'jj' keymap to exit terminal mode

--- init.lua
+++ init.lua
@@ -96,7 +96,6 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("t", "<leader>pv", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set("t", "jj", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
